351e31c0d086bd3448c49809333e3a7711c379ac
ourfor <ourfor@qq.com>
2022-08-26 22:35:29 +0800

    ✨ add info component

---
+++ MediaService/components/OHInfoComponent.h
@@ -0,0 +1,16 @@
+//
+//  OHInfoComponent.h
+//  MediaService
+//
+//  Created by 梁甜 on 2022/8/26.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OHInfoComponent : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MediaService/components/OHInfoComponent.m
@@ -0,0 +1,12 @@
+//
+//  OHInfoComponent.m
+//  MediaService
+//
+//  Created by 梁甜 on 2022/8/26.
+//
+
+#import "OHInfoComponent.h"
+
+@implementation OHInfoComponent
+
+@end
