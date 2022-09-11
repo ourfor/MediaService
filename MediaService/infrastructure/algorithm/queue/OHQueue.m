eea5ace8ee5a7e21aa47ba663f9d9cbf3acd33e5
ourfor <ourfor@qq.com>
2022-09-11 22:13:44 +0800

    ✨ implement queue

--- MediaService/infrastructure/algorithm/queue/OHQueue.m
+++ MediaService/infrastructure/algorithm/queue/OHQueue.m
@@ -6,3 +6,46 @@
 //
 
 #include "OHQueue.h"
+
+void OHQueueInit(OHQueue *queue) {
+    if (!queue) {
+        return;
+    }
+    
+    queue->length = 0;
+    queue->elements = [NSMutableArray arrayWithCapacity:32];
+}
+
+/// return given queue elements size
+int OHQueueLength(OHQueue *queue) {
+    if (!queue) {
+        return 0;
+    }
+    
+    return queue->length;
+}
+
+void OHEnQueue(OHQueue *queue, id element) {
+    if (!queue) {
+        return;
+    }
+    if ([queue->elements isKindOfClass:NSMutableArray.class]) {
+        NSMutableArray *elements = (NSMutableArray *)queue->elements;
+        [elements addObject:element];
+    }
+}
+
+id OHDeQueue(OHQueue *queue) {
+    if (!queue) {
+        return nil;
+    }
+    
+    if ([queue->elements isKindOfClass:NSMutableArray.class]) {
+        NSMutableArray *elements = (NSMutableArray *)queue->elements;
+        id element = [elements firstObject];
+        [elements removeObjectAtIndex:0];
+        return element;
+    }
+    
+    return nil;
+}
